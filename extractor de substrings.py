a8b2eaba71e2c2aa3b864f077b6dcc59dfcb7f07
Thebishopp <alvaroboineau94@gmail.com>
2021-10-20 05:46:47 -0300

    Update extractor de substrings.py

--- extractor de substrings.py
+++ extractor de substrings.py
@@ -1,9 +1,8 @@
 import pandas as pd
 import re
-df = pd.read_csv("calles1.csv", index_col="Id", encoding="latin-1", sep=";")
-df2 = pd.read_excel("direcciones.xlsx")
+df = pd.read_csv("calles1.csv", index_col="Id", encoding="latin-1", sep=";") #DataFrame donde está la celda con las substrings a extraer
+df2 = pd.read_excel("direcciones.xlsx") #DataFrame que se utiliza como direccionario para la comparación con el primer DataFrame
 a = df["Direccion"].to_list()
 b = df2["Descripción"].to_list()
-pd.set_option("display.max_rows", None, "display.max_columns", None)
-c = df2.Descripción.str.extractall(f"({'|'.join(df['Direccion'])})").unstack()
-c.to_csv("colles.csv")
+c = df2.Descripción.str.extractall(f"({'|'.join(df['Direccion'])})").unstack() #Función que extrae las substrings provistas el df2, compara cada ROW con el df2
+c.to_csv("calles.csv") #Guarda en csv las substrings extraidas. 
